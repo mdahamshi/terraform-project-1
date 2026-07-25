8ae0723b351500f11be09f4720fc588eca1d68b0
Mohammad Dahamshi <mohammad.dahamshi@gmail.com>
2026-07-25 05:36:31 +0000

    remove some attachment because of quota

--- roles.tf
+++ roles.tf
@@ -10,15 +10,15 @@ locals {
       "AmazonVPCFullAccess",
       "AmazonEC2FullAccess",
       "AmazonRDSFullAccess",
-      "AmazonS3FullAccess",          # storage access — almost always needed
-      "AWSCloudFormationFullAccess", # if they deploy infra via CFN/CDK
-      "AmazonECS_FullAccess",        # if using containers/ECS
-      "AWSLambda_FullAccess",        # if using serverless functions
-      "CloudWatchFullAccess",        # logs/metrics/alarms — devs need to debug
-      "AmazonSNSFullAccess",         # if app uses pub/sub
-      "AmazonSQSFullAccess",         # if app uses queues
-      "SecretsManagerReadWrite",     # app secrets, DB creds
-      "AmazonElastiCacheFullAccess", # if using Redis/Memcached
+      # "AmazonS3FullAccess",          # storage access — almost always needed
+      # "AWSCloudFormationFullAccess", # if they deploy infra via CFN/CDK
+      # "AmazonECS_FullAccess",        # if using containers/ECS
+      # "AWSLambda_FullAccess",        # if using serverless functions
+      # "CloudWatchFullAccess",        # logs/metrics/alarms — devs need to debug
+      # "AmazonSNSFullAccess",         # if app uses pub/sub
+      # "AmazonSQSFullAccess",         # if app uses queues
+      # "SecretsManagerReadWrite",     # app secrets, DB creds
+      # "AmazonElastiCacheFullAccess", # if using Redis/Memcached
     ]
     auditor = [
       "SecurityAudit"
